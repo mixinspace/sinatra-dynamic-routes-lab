c2a3835ccbd56d93f1de0b78dd3de26c9d78395a
mixinspace <islammadkaur@gmail.com>
2020-11-19 10:46:10 -0600

    commit tree d25c9635100182323a981b738a51504359896aa0

--- app.rb
+++ app.rb
@@ -2,5 +2,35 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get "/reversename/:name" do
+    params[:name].reverse
+  end
+  
+  get "/square/:number" do
+    (params[:number].to_i ** 2).to_s
+  end
+  
+  get "/say/:number/:phrase" do
+    @num = params[:number].to_i
+    params[:phrase] * @num
+  end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+  num1 = params[:number1].to_i
+  num2 = params[:number2].to_i
+  case params[:operation]
+  when 'add'
+    (num1 + num2).to_s
+  when 'subtract'
+    (num2 - num1).to_s
+  when 'multiply'
+    (num2 * num1).to_s
+  when 'divide'
+    (num1 / num2).to_s
+  end
+  end
 end
